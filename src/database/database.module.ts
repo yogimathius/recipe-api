1e88791ee4aab1c92a3d82edc086b1f7afd13a9a
Mathius <info@yogimathius.dev>
2023-02-24 23:44:20 -0800

    chore: add db config (not fully working)

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
-import { Recipe } from 'src/recipe/entities/recipe.entity';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Recipe } from '../recipe/entities/recipe.entity';
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}', Recipe],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
+        imports:[ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'yogimathius', // set the username here
+            password: 'password',
+            database: 'recipe_api',
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+        inject: [ConfigService],
     }),
   ],
 })
