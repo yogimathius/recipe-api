9e12e2adc175185abcc4943d4b57d57d1fe9a86b
Mathius <info@yogimathius.dev>
2023-02-26 23:18:07 -0800

    chore: cleanup recipe entity

--- src/recipe/entities/recipe.entity.ts
+++ src/recipe/entities/recipe.entity.ts
@@ -1,11 +1,10 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
-import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable, OneToMany } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, JoinTable, OneToMany } from 'typeorm';
 import { Ingredient } from './ingredient.entity';
 import { Instruction } from './instruction.entity';
 
 @Entity()
 @ObjectType()
-// @InputType('RecipeInput')
 export class Recipe {
   @PrimaryGeneratedColumn()
   id: number;
@@ -18,11 +17,9 @@ export class Recipe {
 
   @OneToMany(() => Recipe, recipe => recipe.ingredients)
   @JoinTable()
-  @Field(() => Ingredient)
   ingredients: Ingredient[];
 
   @OneToMany(() => Recipe, recipe => recipe.instructions)
   @JoinTable()
-  @Field(() => Instruction)
   instructions: Instruction[];
 }
